1119137b601a5b6e672a31cbc2413de23a0db546
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-02-27 12:58:09 +0100

    fixed a link to creating a book page

--- src/components/BookCreateButton.tsx
+++ src/components/BookCreateButton.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function BookCreateButton() {
   return (
-    <Link href="/create-book">
+    <Link href="/books/create-book">
     <button className="bg-green-600 text-xl mb-6 font-bold text-white px-6 py-3 rounded-lg shadow-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 dark:bg-green-800 dark:hover:bg-green-700 dark:focus:ring-green-400">Create</button>
     </Link>
   );
