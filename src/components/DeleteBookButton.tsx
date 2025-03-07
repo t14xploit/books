06303111487d9f7ba1acf4c16e55a987c20c8946
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-03-07 10:11:16 +0100

    added Pending state to a delete button

--- src/components/DeleteBookButton.tsx
+++ src/components/DeleteBookButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { deleteBook } from "@/actions/book";
+import { useState } from "react";
 
 
 interface DeleteBookButtonProps {
@@ -7,14 +8,18 @@ interface DeleteBookButtonProps {
 }
 
 export default function DeleteBookButton({bookId}:DeleteBookButtonProps) {
+  const [isPending, setIsPending] = useState(false);
+
+  
   async function handleClick(){
     const result = confirm("Are you sure you want to delete this book?");
    if(!result) return;
-   
+   setIsPending(true);
+
     await deleteBook(bookId);
+    setIsPending(false);
   }
     return (
-    <button onClick={handleClick} className="text-4xl text-red-500 hover:text-red-400 p-2 rounded-full transition duration-200">
-    🗑
+    <button onClick={handleClick} className="text-4xl text-red-500 hover:text-red-400 p-2 rounded-full transition duration-200" disabled={isPending}> {isPending? "Deleting...":"🗑"}
   </button>  )
 }
